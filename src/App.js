8db8b2010a964b2a32397c9ebe9ca6657fd814ef
taojian <taojian86@yahoo.com>
2018-11-04 19:47:34 +0800

    创建react项目

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>
+            Kevin
+            </h1>
+            <p1>
+              my name is kevin, i am 8 years old.
+              </p1>
+                          <h1>
+              zoe
+              </h1>
+              <p1>
+                my name is zoe, i am 8 years old.
+                </p1>
+      </div>
+    );
+  }
+}
+
+export default App;
